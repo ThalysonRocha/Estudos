80411c0e0ebefdd8ec15299f9c8fef912e0360f4
Thalyson Rocha <61715894+ThalysonRocha@users.noreply.github.com>
2020-11-24 22:44:10 -0300

    Add files via upload

--- Linguagem de Programação I (2020.2)/ESTRESSE FINAL/projeto.c
+++ Linguagem de Programação I (2020.2)/ESTRESSE FINAL/projeto.c
@@ -9,15 +9,22 @@ struct obra
     double orcamento;
 }obras[10];
 
-int logado = 0;
-int qnt_obra, OPCAO = 1, i, codigobra=1;
-double ORCAMENTO, CAIXA, DESPESAS;
-char historico[999][140], mensagens[999][140];
+struct produto
+{
+  int quantprod;
+  char nomeprod[20];
+} listacompras[10];
 
+int logado = 0;
+int qnt_obra, OPCAO = 1, i, codigobra=1, codigoprod = 1;
+double ORCAMENTO, CAIXA, DESPESAS;
+char historico[999][140], mensagens[999][140], nome[20];
+
+//FUNÇÕES GERAIS
 int login()
 {   
   char login[20], senha[6]; 
-  char id[4][15] = {"gestor\n", "engenheiro", "MESTRE", "FORNECEDOR"}, senhas[4][10] = {"111\n", "222", "333", "444"};
+  char id[5][15] = {"gestor\n", "engenheiro\n", "mestre\n", "fornecedor\n","sair\n"}, senhas[4][10] = {"111\n", "222\n", "333\n", "444\n"};
 
   do{  
     printf("\nLOGIN: ");
@@ -30,20 +37,21 @@ int login()
 
     
     if ((strcmp(login, id[0]) == 0) && (strcmp(senha, senhas[0]) == 0)){
-      printf("\nSeja bem vindo GESTOR !");
       logado = 1;
     }
     else if ((strcmp(login, id[1]) == 0) && (strcmp(senha, senhas[1]) == 0)){
-      printf("\nSeja bem vindo ENGENHEIRO !");
       logado = 2;
     }
     else if ((strcmp(login, id[2]) == 0) && (strcmp(senha, senhas[2]) == 0)){
-      printf("\nSeja bem vindo MESTRE DE OBRAS !");
       logado = 3;
     }
     else if ((strcmp(login, id[3]) == 0) && (strcmp(senha, senhas[3]) == 0)){
       printf("\nSeja bem vindo FORNECEDOR !");
       logado = 4;
+    }
+    else if (strcmp(login, id[4]) == 0){
+      printf("\nPrograma encerrado!");
+      logado = 99;
     }
     else{
       printf("\nLogin e/ou senha invalidos\n");
@@ -56,6 +64,7 @@ int login()
     return logado;
 }
 
+//FUNÇÕES PARA O GESTOR
 char nomeobra()
 {   
   fflush(stdin);
@@ -81,6 +90,7 @@ double orcamentoobra(){
 int gestor ()
 {
   fflush(stdin);
+  printf("\nSeja bem vindo GESTOR !");
   while(OPCAO != 0){
     printf("\nO que deseja fazer ?\n"
     "\nDigite a opcao correspondente:"
@@ -116,7 +126,7 @@ int gestor ()
       printf("Digite a OPCAO desejada: ");
       scanf("%d", &OPCAO);
 
-      printf("\n\nO orçamento inicial para esta obra foi de R$ %.2lf\n", obras[OPCAO].orcamento);
+      printf("\n\nO orcamento inicial para esta obra foi de R$ %.2lf\n", obras[OPCAO].orcamento);
       break;
     
     case  3:
@@ -125,57 +135,140 @@ int gestor ()
       }
       break;
 
-    default:
-      printf("\nVoce nao selecionou nenhuma opcao, atendimento encerrado.");
+    case 0:
+      printf("\nAtendimento encerrado.");
       OPCAO = 0;
-    
-    //qnt_obra, ORCAMENTO;
+
     }
   }
   return 0;
 }
 
+//FUNÇÕES PARA O MESTRE
+char nomeproduto()
+{
+  fflush(stdin);
+  printf("\n\nPor favor digite o nome do produto a ser solicitado: \n");
+  fgets(listacompras[codigoprod].nomeprod, 20, stdin);
+  strcpy(nome, listacompras[codigoprod].nomeprod);
+
+  nome[strcspn(nome, "\n")] = 0;
+  
+  return nome, listacompras[codigoprod].nomeprod;
+  fflush(stdin);
+}
+
+int quantprod()
+{
+  printf("\nQual a quantidade de [ %s ] que voce deseja solicitar ?\nQuantidade: ", nome);
+  scanf("%d", &listacompras[codigoprod].quantprod);
+  system("cls");
+  printf("Produto adicinado a lista.");
+   
+  return listacompras[codigoprod].quantprod;
+  codigoprod++;
+}
+
 int mestre()
 {
+  printf("\nSeja bem vindo MESTRE DE OBRAS !");
   while(OPCAO != 0){
-    printf("O que deseja fazer ?\n"
-    "\nDigite a opção correspondente:"
+    printf("\nO que deseja fazer ?\n"
+    "\nDigite a opcao correspondente:"
     "\n1 - Solicitar a compra de um produto."
     "\n2 - Confirmar recebimento da compra."
     "\n3 - Listar compras efetuadas."
-    "\n4 - Solicitar a contratação de um funcionário.");
+    "\n4 - Solicitar a contratacao de um funcionario."
+    "\n\n0 - Encerrar atendimento. \nOpcao: ");
     scanf("%d", &OPCAO);
+
+    fflush(stdin);
+    switch (OPCAO)
+    {
+    case 1:
+      nomeproduto();
+      quantprod(); 
+      break;
+
+    case 2:
+      break;
+    
+    case  3:
+      break;
+
+    case 0:
+      printf("\nAtendimento encerrado.");
+      OPCAO = 0;
+    
+    }
   }
   return 0;
 }
 
+//FUNÇÕES PARA O ENGENHEIRO
 int engenheiro()
 {
+  printf("\nSeja bem vindo ENGENHEIRO !");
   while(OPCAO != 0){
     printf("O que deseja fazer ?\n"
-    "\nDigite a opção correspondente:"
+    "\nDigite a opcaoo correspondente:"
     "\n1 - Iniciar obra."
-    "\n2 - Contratar funcionários."
+    "\n2 - Contratar funcionÃ¡rios."
     "\n3 - Verificar Custa da obra."
     "\n4 - Selecionar fornecedor e finalizar compra."
-    "\n5 - Adicionar mensagem ao histórico da obra.");
+    "\n5 - Adicionar mensagem ao histÃ³rico da obra.");
     scanf("%d", &OPCAO);
 
+    fflush(stdin);
+    switch (OPCAO)
+    {
+    case 1:
+      break;
+
+    case 2:
+      break;
+    
+    case  3:
+      break;
+
+    case 0:
+      printf("\nAtendimento encerrado.");
+      OPCAO = 0;
+    
+    }
   }
   return 0;
 }
 
 int fornecedor()
 {
+  printf("\nSeja bem vindo FORNECEDOR !");
   while(OPCAO != 0){
     printf("O que deseja fazer ?\n"
-    "\nDigite a opção correspondente:"
-    "\n1 - Visualizar solicitações de compra."
-    "\n2 - Adicionar valor de três fornecedores.");
+    "\nDigite a opÃ§Ã£o correspondente:"
+    "\n1 - Visualizar solicitaÃ§Ãµes de compra."
+    "\n2 - Adicionar valor de trÃªs fornecedores.");
     scanf("%d", &OPCAO);
-  }    
-  return 0;
 
+    fflush(stdin);
+    switch (OPCAO)
+    {
+    case 1:
+      break;
+
+    case 2:
+      break;
+    
+    case  3:
+      break;
+
+    case 0:
+      printf("\nAtendimento encerrado.");
+      OPCAO = 0;
+    
+    }
+  }
+  return 0;
 }
 
 
@@ -183,33 +276,30 @@ int main()
 {
   printf("===PROGRAMA DE LOGIN===\n");
 
+  do{
   login();
-
+  system("cls");
   switch (logado)
   {
   case 1:
-    printf("Seja bem vindo GESTOR");
     gestor();
     break;
   
   case 2:
-    printf("Seja bem vindo ENGENHEIRO");
     engenheiro();
     break;
   
   case 3:
-    printf("Seja bem vindo MESTRE DE OBRAS");
     mestre();
     break;
   
   case 4:
-    printf("Seja bem vindo FORNECEDOR");
     fornecedor();
     break;
-  
+  }
+  }while(logado != 99);
 
   printf("\n\nFIM");
 
   return 0;
-}
 }
